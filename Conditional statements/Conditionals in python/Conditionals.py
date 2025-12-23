35e35e896a863b42d552002b4d2155b9b1d509f6
Gre882at <adeniyigreat798@gmail.com>
2025-12-23 13:04:41 +0100

    Add files via upload

---
+++ Conditional statements/Conditionals in python/Conditionals.py
@@ -0,0 +1,38 @@
+# Example 1: Using score
+score = 75
+if score >= 70:
+    print("Excellent")  # Output: Excellent
+elif score == 15:
+    print("Very Poor")
+else:
+    print("Poor")
+# Example 2: Grade 
+grade = 65
+
+if grade >= 70:
+    print("Distinction")
+elif grade >= 50:
+    print("Credit")  # Output: Credit
+else:
+    print("Fail")
+# Example 3: Using age
+age = 16
+
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teen")  # Output: Teen
+else:
+    print("Adult")
+    # Example 4: Using school_class
+school_class = "JSS3"
+
+if school_class == "JSS1":
+    print("Junior Secondary 1")
+elif school_class == "JSS2":
+    print("Junior Secondary 2")
+elif school_class == "JSS3":
+    print("Junior Secondary 3")  # Output: Junior Secondary 3
+else:
+    print("Unknown Class")
+
